6d047c0910e10f974aedc0c60d727f98a75f92e5
smakivic <smakivic03@gmail.com>
2024-01-17 14:26:48 +0100

    Finished get and post for Android

--- app/src/main/java/com/example/newvoyagerapp/AddVoyagerActivity.java
+++ app/src/main/java/com/example/newvoyagerapp/AddVoyagerActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import com.android.volley.AuthFailureError;
